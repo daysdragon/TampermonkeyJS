4ffc4d5dad396eb6fac3188e5787288dcdc018d8
lzghzr <lzggzr@gmail.com>
2016-03-19 01:32:07 +0800

    添加Steam卡牌脚本

---
+++ SteamCardMaximumProfit/SteamCardMaximumProfit.meta.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name        steam卡牌利润最大化
+// @description 按照美元区出价，最大化steam卡牌卖出的利润
+// @version     0.0.0.4
+// @author      lzzr
+// @namespace   https://lzzr.me/
+// @include     /^https?\:\/\/steamcommunity\.com\/.*\/inventory\/.*$/
+// @downloadURL https://github.com/lzghzr/GreasemonkeyJS/raw/master/SteamCardMaximumProfit/SteamCardMaximumProfit.user.js
+// @updateURL   https://github.com/lzghzr/GreasemonkeyJS/raw/master/SteamCardMaximumProfit/SteamCardMaximumProfit.meta.js
+// @supportURL  https://github.com/lzghzr/GreasemonkeyJS/issues
+// @license     Apache Licence 2.0
+// @compatible  chrome
+// @compatible  firefox
+// @grant       none
+// @run-at      document-end
+// ==/UserScript==
