11a414bed689562c1449bef4f98291bf13326a09
lzghzr <lzggzr@gmail.com>
2016-08-30 14:58:28 +0800

    替换flash弹幕

---
+++ BiLiveNoVIP/BiLiveNoVIP.d.ts
@@ -0,0 +1,114 @@
+interface Window {
+  flash_popularWords(): string[]
+  player_fullwin(full: boolean): void
+  sendBeatStorm(beat: privateBeats): void
+  protocol: Protocol
+  msg_history: {
+    get: () => void
+  }
+  ROOMID: number
+  MASTERID: number
+}
+interface Protocol {
+  DANMU_MSG(danmu: danmuObject): void
+}
+// CommentCoreLibrary
+declare class CommentManager {
+  constructor(HTMLElement: HTMLElement)
+  width: number
+  height: number
+  options: CCLOptions
+  init(): void
+  start(): void
+  clear(): void
+  stop(): void
+  send(CommentObject: CommentObject): void
+}
+interface CommentObject {
+  mode: number
+  text: string
+  shadow?: boolean
+  color?: number
+  size?: number
+}
+interface CCLOptions {
+  global: {
+    scale: number
+    opacity: number
+    className: string
+  }
+  scroll: {
+    scale: number
+    opacity: number
+  }
+  limit: number
+}
+// player_object
+interface playerObject extends HTMLElement {
+  showComments(show: boolean): boolean
+}
+// 设置信息
+interface config {
+  version: number
+  menu: configMenu
+}
+interface configMenu {
+  noVIPIcon: configMenuData
+  noMedalIcon: configMenuData
+  noUserLevelIcon: configMenuData
+  noLiveTitleIcon: configMenuData
+  noSystemMsg: configMenuData
+  noGiftMsg: configMenuData
+  fixTreasure: configMenuData
+  replaceDanmaku: configMenuData
+  popularWords: configMenuData
+  beatStorm: configMenuData
+  closeDanmaku:configMenuData
+}
+interface configMenuData {
+  name: string
+  enable: boolean
+}
+// 弹幕格式
+interface danmuObject {
+  text: string
+  info: [
+    any[],
+    string,
+    [
+      number,
+      string,
+      number
+    ]
+  ]
+  cmd: string
+  color: number
+}
+// 默认节奏
+interface getAllBeats {
+  code: number
+  msg: string
+  data: getAllBeatsData
+}
+interface getAllBeatsData {
+  public: getAllBeatsDataPublic[]
+  private: getAllBeatsDataPrivate
+}
+interface getAllBeatsDataPublic {
+  id: number
+  content: string
+}
+interface getAllBeatsDataPrivate {
+  id: number
+  status: number
+  content: string
+}
+// 自定义节奏
+interface privateBeats {
+  action: string
+  content: string
+  hadJoin: number
+  id: string
+  num: number
+  time: number
+}
--- BiLiveNoVIP/BiLiveNoVIP.user.ts
+++ BiLiveNoVIP/BiLiveNoVIP.user.ts
@@ -1,16 +1,17 @@
 // ==UserScript==
 // @name        bilibili直播净化
 // @namespace   https://github.com/lzghzr/GreasemonkeyJS
-// @version     2.0.4
+// @version     2.0.5
 // @author      lzghzr
 // @description 屏蔽聊天室礼物以及关键字, 净化聊天室环境
 // @supportURL  https://github.com/lzghzr/GreasemonkeyJS/issues
 // @include     /^http:\/\/live\.bilibili\.com\/\d.*$/
+// @require     https://greasyfork.org/scripts/19695-commentcorelibrary/code/CommentCoreLibrary.js?version=125789
 // @license     MIT
 // @grant       none
 // @run-at      document-end
 // ==/UserScript==
-
+/// <reference path="BiLiveNoVIP.d.ts" />
 /**
  * 屏蔽B站直播间聊天室内容
  * 
@@ -18,55 +19,80 @@
  */
 class BiLiveNoVIP {
   constructor() {
-    this.roomID = this.W.ROOMID
     // 加载设置
-    let config = localStorage.getItem('blnvConfig')
-    this.config = (config === null) ? this.defaultConfig : JSON.parse(config)
+    let config = <config>JSON.parse(localStorage.getItem('blnvConfig'))
+    let defaultConfig = this.defaultConfig
+    if (config === null || config.version === undefined || config.version < defaultConfig.version) {
+      for (let x in defaultConfig.menu) {
+        try {
+          defaultConfig.menu[x].enable = config.menu[x].enable
+        }
+        catch (error) {
+          console.error(error)
+        }
+      }
+      this.config = defaultConfig
+    }
+    else {
+      this.config = config
+    }
   }
   private W = window
   private D = document
-  private roomID: number
+  private DANMU_MSG: (danmu: danmuObject) => void
+  private sendBeatStorm: (beat: privateBeats) => void
+  private playerObject: playerObject
+  // CommentCoreLibrary (//github.com/jabbany/CommentCoreLibrary) - Licensed under the MIT license
+  private CM: CommentManager
+  private tempWord = new Set()
   private config: config
-  private defaultConfig = {
-    noVIPIcon: {
-      name: '老爷标签',
-      enable: false
-    },
-    noMedalIcon: {
-      name: '粉丝勋章',
-      enable: false
-    },
-    noUserLevelIcon: {
-      name: '用户等级',
-      enable: false
-    },
-    noLiveTitleIcon: {
-      name: '成就头衔',
-      enable: false
-    },
-    noSystemMsg: {
-      name: '系统消息',
-      enable: false
-    },
-    noGiftMsg: {
-      name: '礼物信息',
-      enable: false
-    },
-    noSuperGift: {
-      name: '礼物连击',
-      enable: false
-    },
-    noSmallGift: {
-      name: '下方礼物',
-      enable: false
-    },
-    fixTreasure: {
-      name: '下移宝箱',
-      enable: false
-    },
-    popularWords: {
-      name: '屏蔽热词',
-      enable: false
+  private defaultConfig: config = {
+    version: 1472476221722,
+    menu: {
+      noVIPIcon: {
+        name: '老爷标签',
+        enable: false
+      },
+      noMedalIcon: {
+        name: '粉丝勋章',
+        enable: false
+      },
+      noUserLevelIcon: {
+        name: '用户等级',
+        enable: false
+      },
+      noLiveTitleIcon: {
+        name: '成就头衔',
+        enable: false
+      },
+      noSystemMsg: {
+        name: '系统消息',
+        enable: false
+      },
+      noGiftMsg: {
+        name: '礼物信息',
+        enable: false
+      },
+      fixTreasure: {
+        name: '下移宝箱',
+        enable: false
+      },
+      replaceDanmaku: {
+        name: '替换弹幕',
+        enable: false
+      },
+      closeDanmaku: {
+        name: '&nbsp;&nbsp;┣关闭弹幕',
+        enable: false
+      },
+      popularWords: {
+        name: '&nbsp;&nbsp;┣屏蔽热词',
+        enable: false
+      },
+      beatStorm: {
+        name: '&nbsp;&nbsp;┗节奏风暴',
+        enable: false
+      }
     }
   }
   /**
@@ -75,7 +101,16 @@ class BiLiveNoVIP {
   public Start() {
     this.AddUI()
     this.ChangeCSS()
-    if (this.config.popularWords.enable) this.PopularWords(true)
+    this.AddDanmaku()
+    this.W.msg_history = {
+      get: () => {
+        if (this.config.menu.replaceDanmaku.enable) {
+          this.ReplaceDanmaku(true)
+          if (this.config.menu.popularWords.enable) this.PopularWords(true)
+          if (this.config.menu.beatStorm.enable) this.BeatStorm(true)
+        }
+      }
+    }
   }
   /**
    * 模拟实时屏蔽
@@ -92,15 +127,13 @@ class BiLiveNoVIP {
     }
     //css内容
     let cssText = ''
-    if (this.config.noVIPIcon.enable) cssText += '#chat-msg-list .vip-icon {display: none !important;}'
-    if (this.config.noMedalIcon.enable) cssText += '#chat-msg-list .medal-icon {display: none !important;}'
-    if (this.config.noUserLevelIcon.enable) cssText += '#chat-msg-list .user-level-icon {display: none !important;}'
-    if (this.config.noLiveTitleIcon.enable) cssText += '#chat-msg-list .live-title-icon {display: none !important;}'
-    if (this.config.noSystemMsg.enable) cssText += '#chat-msg-list .system-msg {display: none !important;} #chat-msg-list .announcement-container {display: none !important;}'
-    if (this.config.noGiftMsg.enable) cssText += '#chat-msg-list .gift-msg {display: none !important;}'
-    if (this.config.noSuperGift.enable) cssText += '#chat-list-ctnr > .super-gift-ctnr {display: none !important;}'
-    if (this.config.noSmallGift.enable) cssText += '#chat-list-ctnr > #gift-msg-1000 {display: none !important;} #chat-list-ctnr > #chat-msg-list {height: 100% !important;}'
-    if (this.config.fixTreasure.enable) cssText += '#player-container > .treasure-box-ctnr {margin: -160px 0 !important;}'
+    if (this.config.menu.noVIPIcon.enable) cssText += '#chat-msg-list .vip-icon {display: none !important;}'
+    if (this.config.menu.noMedalIcon.enable) cssText += '#chat-msg-list .medal-icon {display: none !important;}'
+    if (this.config.menu.noUserLevelIcon.enable) cssText += '#chat-msg-list .user-level-icon {display: none !important;}'
+    if (this.config.menu.noLiveTitleIcon.enable) cssText += '#chat-msg-list .live-title-icon {display: none !important;}'
+    if (this.config.menu.noSystemMsg.enable) cssText += '#chat-msg-list .system-msg, .announcement-container {display: none !important;}'
+    if (this.config.menu.noGiftMsg.enable) cssText += '#chat-msg-list .gift-msg {display: none !important;} #chat-list-ctnr > .super-gift-ctnr, #gift-msg-1000 {display: none !important;} #chat-list-ctnr > #chat-msg-list {height: 100% !important;}'
+    if (this.config.menu.fixTreasure.enable) cssText += '#player-container > .treasure-box-ctnr {margin: -160px 0 !important;}'
     elmStyle.innerHTML = cssText
   }
   /**
@@ -116,14 +149,14 @@ class BiLiveNoVIP {
     // 传说中的UI, 真的很丑
     let elmDivGun = this.D.createElement('div')
     elmDivGun.id = 'gunBut'
-    let html = '滚<div id="gunMeun" class="gunHide">'
+    let html = '滚<div id="gunMenu" class="gunHide">'
     // 循环插入内容
-    for (let x in this.config) {
+    for (let x in this.config.menu) {
       html += `
 <div>
   <input type="checkbox" id="${x}" class="gunHide" />
 	<label for="${x}"></label>
-  <span>${this.config[x].name}</span>
+  <span>${this.config.menu[x].name}</span>
 </div>`
     }
     html += '</div>'
@@ -131,36 +164,219 @@ class BiLiveNoVIP {
     // 插入菜单按钮
     elmDivBtns.appendChild(elmDivGun)
     // 获取刚刚插入的DOM
-    let elmDivMeun = this.D.querySelector('#gunMeun')
+    let elmDivMenu = this.D.querySelector('#gunMenu')
     // 为了和b站更搭, 所以监听body的click
     this.D.body.addEventListener('click', (ev) => {
       let evt = <HTMLElement>ev.target
       if (elmDivGun.contains(evt)) {
-        if (evt === elmDivGun) {
-          elmDivMeun.classList.toggle('gunHide')
-        }
+        if (evt === elmDivGun) elmDivMenu.classList.toggle('gunHide')
       }
       else {
-        elmDivMeun.classList.add('gunHide')
+        elmDivMenu.classList.add('gunHide')
       }
     })
     // 循环设置监听插入的DOM
-    for (let x in this.config) {
+    let replaceDanmakuCheckbox = <HTMLInputElement>this.D.querySelector('#replaceDanmaku')
+    let closeDanmakuCheckbox = <HTMLInputElement>this.D.querySelector('#closeDanmaku')
+    let popularWordsCheckbox = <HTMLInputElement>this.D.querySelector('#popularWords')
+    let beatStormCheckbox = <HTMLInputElement>this.D.querySelector('#beatStorm')
+    for (let x in this.config.menu) {
       let checkbox = <HTMLInputElement>this.D.getElementById(x)
-      checkbox.checked = this.config[x].enable
+      checkbox.checked = this.config.menu[x].enable
       checkbox.addEventListener('change', (ev) => {
         let evt = <HTMLInputElement>ev.target
-        this.config[evt.id].enable = checkbox.checked
+        this.config.menu[evt.id].enable = evt.checked
         localStorage.setItem('blnvConfig', JSON.stringify(this.config))
-        if (evt.id === 'popularWords') {
-          this.PopularWords(checkbox.checked)
-        }
-        else {
-          this.ChangeCSS()
+        switch (evt.id) {
+          case 'replaceDanmaku':
+            this.ReplaceDanmaku(evt.checked)
+            if (!evt.checked) {
+              if (popularWordsCheckbox.checked = true) popularWordsCheckbox.click()
+              if (beatStormCheckbox.checked = true) beatStormCheckbox.click()
+            }
+            break
+          case 'closeDanmaku':
+            if (evt.checked && !replaceDanmakuCheckbox.checked) replaceDanmakuCheckbox.click()
+            break
+          case 'popularWords':
+            this.PopularWords(evt.checked)
+            if (evt.checked && !replaceDanmakuCheckbox.checked) replaceDanmakuCheckbox.click()
+            break
+          case 'beatStorm':
+            this.BeatStorm(evt.checked)
+            if (evt.checked && !replaceDanmakuCheckbox.checked) replaceDanmakuCheckbox.click()
+            break
+          default:
+            this.ChangeCSS()
+            break
         }
       })
     }
   }
+  /**
+   * 添加弹幕层
+   * 
+   * @private
+   */
+  private AddDanmaku() {
+    // 获取播放器节点
+    this.playerObject = <playerObject>this.D.querySelector('#player_object')
+    // 创建弹幕层
+    let danmaku = this.D.createElement('div')
+    danmaku.className = 'gunDanmaku'
+    let danmakuContainer = this.D.createElement('div')
+    danmakuContainer.className = 'gunDanmakuContainer'
+    // 插入弹幕层
+    danmaku.appendChild(danmakuContainer)
+    this.playerObject.parentNode.appendChild(danmaku)
+    this.CM = new CommentManager(danmakuContainer)
+    this.CM.init()
+    // 透明度
+    this.CM.options.scroll.opacity = parseInt(localStorage.getItem('danmuAlpha')) / 100
+    // 存在时间7s
+    this.CM.options.scroll.scale = 1.75
+    // 弹幕密度
+    this.CM.options.limit = parseDensity(localStorage.getItem('danmuDensity'))
+    // 监听视频窗口大小
+    let bodyObserver = new MutationObserver(() => {
+      this.CM.width = danmaku.clientWidth
+      this.CM.height = danmaku.clientHeight
+    })
+    bodyObserver.observe(document.body, { attributes: true, attributeFilter: ['class'] })
+    this.W.addEventListener('resize', () => {
+      this.CM.width = danmaku.clientWidth
+      this.CM.height = danmaku.clientHeight
+    })
+    // 控制条
+    this.D.querySelector('#danmu-alpha-ctrl').addEventListener('input',
+      (ev) => {
+        this.CM.options.scroll.opacity = parseInt((<HTMLInputElement>ev.target).value) / 100
+      }
+    )
+    this.D.querySelector('#danmu-density-ctrl').addEventListener('input',
+      (ev) => {
+        this.CM.options.limit = parseDensity((<HTMLInputElement>ev.target).value)
+      })
+    /**
+     * 计算弹幕密度
+     * 
+     * @param {string} density
+     * @returns {number}
+     */
+    function parseDensity(density: string) {
+      let limit: number
+      switch (density) {
+        case '81':
+          limit = 100
+          break
+        case '82':
+          limit = 200
+          break
+        case '83':
+          limit = 300
+          break
+        case '84':
+          limit = 400
+          break
+        case '85':
+          limit = 500
+          break
+        case '86':
+          limit = 0
+          break
+        default:
+          limit = parseInt(density)
+          break
+      }
+      return limit
+    }
+  }
+  /**
+   * 替换弹幕
+   * 
+   * @private
+   * @param {boolean} enable
+   */
+  private ReplaceDanmaku(enable: boolean) {
+    if (enable) {
+      this.CM.start()
+      // 替换弹幕
+      this.playerObject.showComments(false)
+      let masterID = this.W.MASTERID
+      // 获取聊天信息
+      this.DANMU_MSG = this.W.protocol.DANMU_MSG
+      this.W.protocol.DANMU_MSG = (json: danmuObject) => {
+        if (this.config.menu.closeDanmaku.enable) {
+          this.DANMU_MSG(json)
+        }
+        else {
+          let chatText = json.info[1]
+          if (!this.tempWord.has(chatText)) {
+            let danmuColor = 16777215
+            if (json.info[2][2] === 1) danmuColor = (json.info[2][0] === masterID) ? 6737151 : 16750592
+            let danmu = {
+              mode: 1,
+              text: chatText,
+              size: 0.25 * localStorage.getItem('danmuSize'),
+              color: danmuColor,
+              shadow: true
+            }
+            this.CM.send(danmu)
+            this.DANMU_MSG(json)
+          }
+        }
+      }
+    }
+    else {
+      this.W.protocol.DANMU_MSG = this.DANMU_MSG
+      this.W.msg_history = undefined
+      this.CM.stop()
+      this.CM.clear()
+      this.playerObject.showComments(true)
+    }
+  }
+  /**
+   * 屏蔽热词
+   * 
+   * @private
+   * @param {boolean} disable
+   */
+  private PopularWords(disable: boolean) {
+    let popularWords = this.W.flash_popularWords()
+    for (let y of popularWords) {
+      (disable) ? this.tempWord.add(y) : this.tempWord.delete(y)
+    }
+  }
+  /**
+   * 屏蔽节奏风暴
+   * 
+   * @private
+   * @param {boolean} disable
+   */
+  private BeatStorm(disable: boolean) {
+    let getAllBeats = '/api/ajaxGetAllBeats'
+    this.XHR<getAllBeats>(getAllBeats, 'json')
+      .then((resolve) => {
+        let publicBeats = resolve.data.public
+        for (let y of publicBeats) {
+          (disable) ? this.tempWord.add(y.content) : this.tempWord.delete(y.content)
+        }
+      })
+    if (disable) {
+      this.sendBeatStorm = this.W.sendBeatStorm
+      this.W.sendBeatStorm = (json: privateBeats) => {
+        this.tempWord.add(json.content)
+      }
+    }
+    else {
+      this.W.sendBeatStorm = this.sendBeatStorm
+    }
+  }
+  /**
+   * 添加样式
+   * 
+   * @private
+   */
   private AddCSS() {
     let cssText = `
 .gunHide {
@@ -178,20 +394,20 @@ class BiLiveNoVIP {
   width: 18px;
   vertical-align: text-top;
 }
-#gunBut > #gunMeun {
+#gunBut > #gunMenu {
   animation:move-in-right cubic-bezier(.22,.58,.12,.98) .4s;
   background-color: #fff;
   border-radius: 5px;
   box-shadow: 0 0 2em .1em rgba(0,0,0,0.15);
   cursor: default;
   font-size: 12px;
-  height: 210px;
+  height: 230px;
   margin: -250px -125px;
   padding: 10px;
   position: absolute;
   width: 100px;
 }
-#gunBut > #gunMeun > div {
+#gunBut > #gunMenu > div {
 	background: darkgray;
 	border-radius: 5px;
 	height: 10px;
@@ -199,7 +415,7 @@ class BiLiveNoVIP {
 	position: relative;
 	width: 20px;
 }
-#gunBut > #gunMeun > div > label {
+#gunBut > #gunMenu > div > label {
 	background: dimgray;
 	border-radius: 50%;
 	cursor: pointer;
@@ -211,48 +427,69 @@ class BiLiveNoVIP {
 	transition: all .5s ease;
 	width: 16px;
 }
-#gunBut > #gunMeun > div > input[type=checkbox]:checked + label {
+#gunBut > #gunMenu > div > input[type=checkbox]:checked + label {
   background: #4fc1e9;
 	left: 7px;
 }
-#gunBut > #gunMeun > div > span {
+#gunBut > #gunMenu > div > span {
   left: 0;
   margin: -3px 0 0 20px;
   position: absolute;
   width: 80px;
 }
+.gunDanmaku {
+  position:absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 93%;
+  overflow: hidden;
+  z-index: 10;
+  cursor: pointer;
+  pointer-events: none;
+}
+.gunDanmaku .gunDanmakuContainer {
+  transform: matrix3d(1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1);
+  position: absolute;
+  display: block;
+  overflow: hidden;
+  margin: 0;
+  border: 0;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  z-index: 9999;
+  touch-callout: none;
+  user-select: none;
+}
+.gunDanmaku .gunDanmakuContainer .cmt {
+  transform: matrix3d(1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1);
+  transform-origin: 0% 0%;
+  position: absolute;
+  padding: 3px 0 0 0;
+  margin: 0;
+  color: #fff;
+  font-family: "Microsoft YaHei", SimHei;
+  font-size: 25px;
+  text-decoration: none;
+  text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;
+  text-size-adjust: none;
+  line-height: 100%;
+  letter-spacing: 0;
+  word-break: keep-all;
+  white-space: pre;
+}
+.gunDanmaku .gunDanmakuContainer .cmt.noshadow {
+  text-shadow: none;
+}
+.gunDanmaku .gunDanmakuContainer .cmt.rshadow {
+  text-shadow: -1px 0 white, 0 1px white, 1px 0 white, 0 -1px white;
 }`
     // 插入css
     let elmStyle = this.D.createElement('style')
     elmStyle.innerHTML = cssText
     this.D.body.appendChild(elmStyle)
-  }
-  /**
-   * 屏蔽热词
-   * 
-   * @private
-   */
-  private PopularWords(disable: boolean) {
-    let player = `/api/player?id=cid:${this.roomID}&ts=${Date.now().toString(16)}`
-    let getAllBeats = '/api/ajaxGetAllBeats'
-    let popularWords = this.W.flash_popularWords()
-    this.XHR<getAllBeats>(getAllBeats, 'json')
-      .then((resolve) => {
-        let publicBeats = resolve.data.public
-        for (let y of publicBeats) {
-          popularWords.push(y.content)
-        }
-        return this.XHR<Document>(player, 'document')
-      })
-      .then((resolve) => {
-        let keyword = resolve.querySelector('user_sheid_keyword').innerHTML
-        let userKeyword = new Set(keyword.split(','))
-        let hotWords = [...popularWords].filter((y) => (disable) ? !userKeyword.has(y) : userKeyword.has(y))
-        for (let y of hotWords) {
-          let shield = `/liveact/shield_keyword?keyword=${encodeURIComponent(y)}&roomid=${this.roomID}&type=${(disable) ? 1 : 0}`
-          this.XHR(shield, '', 'POST')
-        }
-      })
   }
   /**
    * 使用Promise封装xhr
@@ -272,15 +509,16 @@ class BiLiveNoVIP {
       let path = url
       if (type === 'jsonp') {
         // 感觉引入jquery还是太大材小用
+        let cbRandom = Math.floor(Math.random() * 1e15)
         let elmScript = this.D.createElement('script')
         this.D.body.appendChild(elmScript)
-        this.W['cb'] = (json) => {
+        this.W[`cb${cbRandom}`] = (json) => {
           clearTimeout(timeout)
           this.D.body.removeChild(elmScript)
-          this.W['cb'] = undefined
+          this.W[`cb${cbRandom}`] = undefined
           resolve(json)
         }
-        elmScript.src = `${path}&callback=cb & _=${Date.now()} `
+        elmScript.src = `${path}&callback=cb${cbRandom}&_=${Date.now()} `
       }
       else {
         let postData = ''
@@ -304,44 +542,3 @@ class BiLiveNoVIP {
 }
 const gun = new BiLiveNoVIP()
 gun.Start()
-
-// 防止类型错误
-interface Window {
-  flash_popularWords(): string[]
-  ROOMID: number
-}
-interface config {
-  noVIPIcon: configData
-  noMedalIcon: configData
-  noUserLevelIcon: configData
-  noLiveTitleIcon: configData
-  noSystemMsg: configData
-  noGiftMsg: configData
-  noSuperGift: configData
-  noSmallGift: configData
-  fixTreasure: configData
-  popularWords: configData
-}
-interface configData {
-  name: string
-  enable: boolean
-}
-
-interface getAllBeats {
-  code: number
-  msg: string
-  data: getAllBeatsData
-}
-interface getAllBeatsData {
-  public: getAllBeatsDataPublic[]
-  private: getAllBeatsDataPrivate
-}
-interface getAllBeatsDataPublic {
-  id: number
-  content: string
-}
-interface getAllBeatsDataPrivate {
-  id: number
-  status: number
-  content: string
-}
