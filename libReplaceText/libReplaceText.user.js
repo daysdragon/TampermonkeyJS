d618762a10fc25ad7384f5faa48adb7c4b67e522
lzgtzr <lzggzr@gmail.com>
2018-10-04 20:10:55 +0800

    libReplaceText

---
+++ libReplaceText/libReplaceText.user.js
@@ -0,0 +1,69 @@
+// ==UserScript==
+// @name         替换文本
+// @namespace    https://github.com/lzghzr/TampermonkeyJS
+// @version      0.0.1
+// @author       lzghzr
+// @description  替换网页内文本, 达到本地化的目的
+// @grant        none
+// @run-at       document-end
+// ==/UserScript==
+const W = typeof unsafeWindow === 'undefined' ? window : unsafeWindow;
+class ReplaceText {
+    constructor(i18n) {
+        this.alert = W.alert.bind(W);
+        this.confirm = W.confirm.bind(W);
+        this.prompt = W.prompt.bind(W);
+        this.i18n = new Map(i18n);
+        const bodyObserver = new MutationObserver(mutations => {
+            mutations.forEach(mutation => {
+                mutation.addedNodes.forEach(addedNode => this.replaceText(addedNode));
+            });
+        });
+        bodyObserver.observe(document.body, { childList: true, subtree: true });
+        this.replaceAlert();
+        this.replaceText(document.body);
+    }
+    replaceAlert() {
+        W.alert = (message) => {
+            if (this.i18n.has(message))
+                message = this.i18n.get(message);
+            return this.alert(message);
+        };
+        W.confirm = (message) => {
+            if (this.i18n.has(message))
+                message = this.i18n.get(message);
+            return this.confirm(message);
+        };
+        W.prompt = (message, _default) => {
+            if (this.i18n.has(message))
+                message = this.i18n.get(message);
+            return this.prompt(message, _default);
+        };
+    }
+    replaceText(node) {
+        this.nodeForEach(node).forEach(textNode => {
+            if (textNode instanceof Text && this.i18n.has(textNode.data))
+                textNode.data = this.i18n.get(textNode.data);
+            else if (textNode instanceof HTMLInputElement) {
+                if (textNode.type === 'button' && this.i18n.has(textNode.value))
+                    textNode.value = this.i18n.get(textNode.value);
+                else if (textNode.type === 'text' && this.i18n.has(textNode.placeholder))
+                    textNode.placeholder = this.i18n.get(textNode.placeholder);
+            }
+        });
+    }
+    nodeForEach(node) {
+        const list = [];
+        if (node.childNodes.length === 0)
+            list.push(node);
+        else {
+            node.childNodes.forEach(child => {
+                if (child.childNodes.length === 0)
+                    list.push(child);
+                else
+                    list.push(...this.nodeForEach(child));
+            });
+        }
+        return list;
+    }
+}
---
+++ libReplaceText/libReplaceText.user.ts
@@ -0,0 +1,96 @@
+// ==UserScript==
+// @name         替换文本
+// @namespace    https://github.com/lzghzr/TampermonkeyJS
+// @version      0.0.1
+// @author       lzghzr
+// @description  替换网页内文本, 达到本地化的目的
+// @grant        none
+// @run-at       document-end
+// ==/UserScript==
+export { }
+
+const W = typeof unsafeWindow === 'undefined' ? window : unsafeWindow
+// @ts-ignore
+class ReplaceText {
+  /**
+   * Creates an instance of ReplaceText.
+   * @param {[string, string][]} i18n
+   * @memberof ReplaceText
+   */
+  constructor(i18n: [string, string][]) {
+    this.i18n = new Map(i18n)
+    const bodyObserver = new MutationObserver(mutations => {
+      mutations.forEach(mutation => {
+        mutation.addedNodes.forEach(addedNode => this.replaceText(addedNode))
+      })
+    })
+    bodyObserver.observe(document.body, { childList: true, subtree: true })
+    this.replaceAlert()
+    this.replaceText(document.body)
+  }
+  public i18n: Map<string, string>
+  public alert = W.alert.bind(W)
+  public confirm = W.confirm.bind(W)
+  public prompt = W.prompt.bind(W)
+  /**
+   * 替换弹出框
+   *
+   * @memberof ReplaceText
+   */
+  public replaceAlert() {
+    W.alert = (message: string) => {
+      // @ts-ignore Map.has is true
+      if (this.i18n.has(message)) message = this.i18n.get(message)
+      return this.alert(message)
+    }
+    W.confirm = (message: string) => {
+      // @ts-ignore Map.has is true
+      if (this.i18n.has(message)) message = this.i18n.get(message)
+      return this.confirm(message)
+    }
+    W.prompt = (message: string, _default: string) => {
+      // @ts-ignore Map.has is true
+      if (this.i18n.has(message)) message = this.i18n.get(message)
+      return this.prompt(message, _default)
+    }
+  }
+  /**
+   * 替换节点上的所有文本节点
+   *
+   * @param {Node} node
+   * @memberof ReplaceText
+   */
+  public replaceText(node: Node) {
+    this.nodeForEach(node).forEach(textNode => {
+      if (textNode instanceof Text && this.i18n.has(textNode.data))
+        // @ts-ignore Map.has is true
+        textNode.data = this.i18n.get(textNode.data)
+      else if (textNode instanceof HTMLInputElement) {
+        if (textNode.type === 'button' && this.i18n.has(textNode.value))
+          // @ts-ignore Map.has is true
+          textNode.value = this.i18n.get(textNode.value)
+        else if (textNode.type === 'text' && this.i18n.has(textNode.placeholder))
+          // @ts-ignore Map.has is true
+          textNode.placeholder = this.i18n.get(textNode.placeholder)
+      }
+    })
+  }
+  /**
+   * 深度遍历节点, 返回文本节点
+   *
+   * @param {Node} node
+   * @returns {Node[]}
+   * @memberof ReplaceText
+   */
+  public nodeForEach(node: Node): Node[] {
+    const list: Node[] = []
+    if (node.childNodes.length === 0) list.push(node)
+    else {
+      node.childNodes.forEach(child => {
+        if (child.childNodes.length === 0) list.push(child)
+        else list.push(...this.nodeForEach(child))
+      })
+    }
+    return list
+  }
+}
