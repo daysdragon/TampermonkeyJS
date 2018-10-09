cf7d8417850f30e62bb4e01f2cc1a5e3d8502fd4
lzgtzr <lzggzr@gmail.com>
2018-10-10 00:03:18 +0800

    I am Groot!

--- iamgroot/iamgroot.user.js
+++ iamgroot/iamgroot.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name        I am Groot!
 // @namespace   https://github.com/lzghzr/TampermonkeyJS
-// @version     0.0.3
+// @version     0.0.4
 // @author      lzghzr
 // @description I am Groot!
 // @match       http://*/*
 // @match       https://*/*
-// @require     https://github.com/lzghzr/TampermonkeyJS/raw/master/libReplaceText/libReplaceText.user.js?v=0.0.5
+// @require     https://github.com/lzghzr/TampermonkeyJS/raw/master/libReplaceText/libReplaceText.user.js?v=0.0.6
 // @license     MIT
 // @grant       none
 // @run-at      document-start
 // ==/UserScript==
-new ReplaceText([[/.*/g, 'I am Groot!']], 'match');
+new ReplaceText([[/^[\s\S]*$/g, 'I am Groot!']], 'match');
--- iamgroot/iamgroot.user.ts
+++ iamgroot/iamgroot.user.ts
@@ -1,16 +1,16 @@
 // ==UserScript==
 // @name        I am Groot!
 // @namespace   https://github.com/lzghzr/TampermonkeyJS
-// @version     0.0.3
+// @version     0.0.4
 // @author      lzghzr
 // @description I am Groot!
 // @match       http://*/*
 // @match       https://*/*
-// @require     https://github.com/lzghzr/TampermonkeyJS/raw/master/libReplaceText/libReplaceText.user.js?v=0.0.5
+// @require     https://github.com/lzghzr/TampermonkeyJS/raw/master/libReplaceText/libReplaceText.user.js?v=0.0.6
 // @license     MIT
 // @grant       none
 // @run-at      document-start
 // ==/UserScript==
 import ReplaceText from '../libReplaceText/libReplaceText.user'
 
-new ReplaceText([[/.*/g, 'I am Groot!']], 'match')
+new ReplaceText([[/^[\s\S]*$/g, 'I am Groot!']], 'match')
