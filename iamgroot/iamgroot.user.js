b7364af618abf15683fe83534a5553ca90395422
lzgtzr <lzggzr@gmail.com>
2018-10-09 22:56:57 +0800

    I am Groot!

--- iamgroot/iamgroot.user.js
+++ iamgroot/iamgroot.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name        I am Groot!
 // @namespace   https://github.com/lzghzr/TampermonkeyJS
-// @version     0.0.2
+// @version     0.0.3
 // @author      lzghzr
 // @description I am Groot!
 // @match       http://*/*
 // @match       https://*/*
-// @require     https://github.com/lzghzr/TampermonkeyJS/raw/master/libReplaceText/libReplaceText.user.js
+// @require     https://github.com/lzghzr/TampermonkeyJS/raw/master/libReplaceText/libReplaceText.user.js?v=0.0.5
 // @license     MIT
 // @grant       none
 // @run-at      document-start
