fe2340677328762f9d6e9686603e9781d69cd3c9
lzgtzr <lzggzr@gmail.com>
2020-08-08 22:02:37 +0800

    [libWasmHash] 支持更多别名

--- libWasmHash/libWasmHash.d.ts
+++ libWasmHash/libWasmHash.d.ts
@@ -1,6 +1,7 @@
 interface InitOutput {
   readonly memory: WebAssembly.Memory
   readonly hash: (a: number, b: number, c: number, d: number, e: number) => void
+  readonly __wbindgen_export_0: WebAssembly.Global
   readonly __wbindgen_malloc: (a: number) => number
   readonly __wbindgen_realloc: (a: number, b: number, c: number) => number
   readonly __wbindgen_free: (a: number, b: number) => void
