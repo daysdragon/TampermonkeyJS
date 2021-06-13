cc6f5857d594411bca075b0ae1f4a5a4d3a97257
lzgtzr <lzggzr@gmail.com>
2021-06-13 19:22:40 +0800

    [FuckBaiduPan] 兼容新版

--- FuckBaiduPan/FuckBaiduPan.user.js
+++ FuckBaiduPan/FuckBaiduPan.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        FuckBaiduPan
 // @namespace   https://github.com/lzghzr/TampermonkeyJS
-// @version     0.0.2
+// @version     0.0.3
 // @author      lzghzr
 // @description FuckBaiduPan
 // @supportURL  https://github.com/lzghzr/TampermonkeyJS/issues
@@ -9,22 +9,18 @@
 // @match       *://yun.baidu.com/*
 // @license     MIT
 // @grant       none
-// @run-at      document-start
+// @run-at      document-end
 // ==/UserScript==
 const W = typeof unsafeWindow === 'undefined' ? window : unsafeWindow;
-Reflect.defineProperty(W.navigator, 'platform', { value: 'iPad' });
-W.addEventListener('load', () => {
-    const observer = new MutationObserver(mutations => {
-        mutations.forEach(mutation => {
-            mutation.addedNodes.forEach(addedNode => {
-                if (addedNode.id === 'share') {
-                    const share = W.require(['function-widget-1:share/util/shareFriend/createLinkShare.js']);
-                    Reflect.defineProperty(share.prototype, 'makePrivatePassword', {
-                        value: () => prompt('请输入自定义的密码', 'null')
-                    });
-                }
+W.require = new Proxy(W.require, {
+    apply: function (target, _this, args) {
+        if (args[0] === 'function-widget-1:share/util/newShare/linkSetting.js') {
+            const share = target([...args]);
+            Reflect.defineProperty(share, 'makePrivatePassword', {
+                value: () => prompt('请输入自定义的密码', 'null')
             });
-        });
-    });
-    observer.observe(document.body, { childList: true });
+            return share;
+        }
+        return Reflect.apply(target, _this, args);
+    }
 });
--- FuckBaiduPan/FuckBaiduPan.user.ts
+++ FuckBaiduPan/FuckBaiduPan.user.ts
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        FuckBaiduPan
 // @namespace   https://github.com/lzghzr/TampermonkeyJS
-// @version     0.0.2
+// @version     0.0.3
 // @author      lzghzr
 // @description FuckBaiduPan
 // @supportURL  https://github.com/lzghzr/TampermonkeyJS/issues
@@ -9,26 +9,22 @@
 // @match       *://yun.baidu.com/*
 // @license     MIT
 // @grant       none
-// @run-at      document-start
+// @run-at      document-end
 // ==/UserScript==
 /// <reference path="FuckBaiduPan.d.ts" />
 export { }
 
 const W = typeof unsafeWindow === 'undefined' ? window : unsafeWindow
-Reflect.defineProperty(W.navigator, 'platform', { value: 'iPad' })
-W.addEventListener('load', () => {
-  const observer = new MutationObserver(mutations => {
-    mutations.forEach(mutation => {
-      mutation.addedNodes.forEach(addedNode => {
-        if ((<HTMLElement>addedNode).id === 'share') {
-          // https://greasyfork.org/zh-CN/scripts/36114
-          const share = W.require(['function-widget-1:share/util/shareFriend/createLinkShare.js'])
-          Reflect.defineProperty(share.prototype, 'makePrivatePassword', {
-            value: () => prompt('请输入自定义的密码', 'null')
-          })
-        }
+
+W.require = new Proxy(W.require, {
+  apply: function (target, _this, args) {
+    if (args[0] === 'function-widget-1:share/util/newShare/linkSetting.js') {
+      const share = target([...args])
+      Reflect.defineProperty(share, 'makePrivatePassword', {
+        value: () => prompt('请输入自定义的密码', 'null')
       })
-    })
-  })
-  observer.observe(document.body, { childList: true })
+      return share
+    }
+    return Reflect.apply(target, _this, args)
+  }
 })
